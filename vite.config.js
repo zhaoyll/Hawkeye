5318183eab2d3927c8a3f039621a7ccdcb4a5452
zhaoyll <1308032908@qq.com>
2026-05-15 12:55:12 +0800

    main

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import obfuscator from 'rollup-plugin-obfuscator'; // 导入代码混淆插件
 
-module.exports = {
-  publicPath: '/Hawkeye/'
-}
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
